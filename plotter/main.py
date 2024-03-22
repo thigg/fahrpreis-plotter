0bd812324ae09374c5a3d200758da31ab25f23cf
thigg <thigg-github@kogge.net>
2024-03-22 14:40:57 +0100

    using plotly to render the graph

--- plotter/main.py
+++ plotter/main.py
@@ -5,11 +5,12 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 from typing import Dict, List
 
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plt
+import plotly.express as px
+from plotly.graph_objs import Figure
 
 parser = argparse.ArgumentParser(description='accumulate fahrpreis data')
 parser.add_argument('--dbfile', help='path to the sqlitefile with the data', required=True)
+parser.add_argument('--output_file', help='if set, writes the html to the given file')
 parser.add_argument('--start_station', help='start station id', required=True)
 parser.add_argument('--end_station', help='end station id', required=True)
 parser.add_argument('--plot_timeframe_past', help='oldest travel start date on the plot, days relative to now',
@@ -48,52 +49,54 @@ def accumulate_sqlite(filename: str, start_station: int, end_station: int, timef
     return prices_dict
 
 
-def plot(result: Dict[str, List[tuple[int, int]]]):
+def plot(result: Dict[str, List[tuple[int, int]]], x_day_range_past: int, x_day_range_future: int, num_bins_per_day: int = 24 / 3) -> Figure:
     """
-
     :param result: the data to ingest for the plot
-    :return: shows the plot
+    :param x_day_range_past: how many days before departure to display
+    :param x_day_range_future: how many days after departure to display
+    :param num_bins_per_day: how many prices to consider per day, should match the query interval (defaults to every 3 hours)
     """
-    time_to_departure = []
-    # booking_date = []
-    departure_date = []
-    # y_axis2 = []
-    travel_price_euro = []
-    # z_axis2 = []
+    queried_at_num_bins = int((x_day_range_past+x_day_range_future) * num_bins_per_day)
+    queried_at_departure_bin_index = queried_at_num_bins - int((x_day_range_future) * num_bins_per_day)
+    pricemap: Dict[datetime, List[float]] = defaultdict(lambda: [-1] * (queried_at_num_bins))
+
     for i, travelprices in enumerate(result.items()):
         try:
             when, price_records = travelprices
-            starttime = datetime.fromtimestamp(int(when) / 1000)
-            # z_axis2.append((endtime-starttime).total_seconds())
-            # y_axis2.append(starttime)
+            starttime: datetime = datetime.fromtimestamp(int(when) / 1000)
             price_record: Dict[str, int]
             for queried_at, price in price_records:
                 queried_at_date = datetime.fromtimestamp(queried_at / 1000)
                 days_to_departure = (starttime - queried_at_date).total_seconds() / (60 * 60 * 24)
-                time_to_departure.append(-days_to_departure)
-                departure_date.append(starttime)
-                travel_price_euro.append(price / 100)
+                queried_at_bin_index = queried_at_departure_bin_index - int(days_to_departure * num_bins_per_day)
+                if queried_at_bin_index >= queried_at_num_bins or queried_at_bin_index < 0:
+                    # discard datapoint because out of rendering range
+                    continue
+                try:
+                    pricemap[starttime][queried_at_bin_index] = price / 100
+                except:
+                    print(f"{queried_at_bin_index} {days_to_departure} {starttime} {queried_at_date}")
+                    raise
         except:
             logging.exception("exception while prepping plot %d %s", i, travelprices)
-    print("resulting datapoints: %d" % len(travel_price_euro))
-    plt.rcParams['figure.figsize'] = [12, 20]
-    fig, ax = plt.subplots(1)
-    price_records = ax
-    # plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%a %d.%m'))
-    plt.gca().yaxis.set_major_formatter(mdates.DateFormatter('%a %d.%m'))
-    # plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    plt.gca().yaxis.set_major_locator(mdates.DayLocator())
-    pcm = price_records.scatter(time_to_departure, departure_date, c=travel_price_euro, cmap=plt.colormaps["plasma"], marker=".", s=1,
-                                vmax=75)
-    # fig.autofmt_xdate()
-    # prices.twinx().barh(y_axis2,z_axis2,height=0.1)
-    fig.colorbar(pcm, label="price (Euro)", ax=price_records)
-    plt.gca().xaxis.set_label_text("tage bis abfahrt / wann ich buche")
-    plt.gca().yaxis.set_label_text("datum der reise / wann ich fahren möchte")
-    plt.show()
+
+    sorted_pricemap = sorted(pricemap.items())
+    fig = px.imshow([x[1] for x in sorted_pricemap],
+                    labels=dict(x="tage bis abfahrt / wann ich buche", y="datum der reise / wann ich fahren möchte",
+                                color="Preis (Euro)"),
+                    x=[f"{-(x-queried_at_departure_bin_index) / num_bins_per_day:.2f}" for x in range(int(queried_at_num_bins))],
+                    y=[k.strftime('%A %d-%m-%Y, %H:%M') for k, v in sorted_pricemap]
+                    )
+    return fig
 
 
 timeframe_start: datetime = datetime.now() - timedelta(days=args.plot_timeframe_past)
 timeframe_end: datetime = datetime.now() + timedelta(days=args.plot_timeframe_future)
 result = accumulate_sqlite(args.dbfile, int(args.start_station), int(args.end_station), timeframe_start, timeframe_end)
-plot(result)
+figure = plot(result, 60, 1)
+if args.output_file:
+    html = figure.to_html()
+    with open(args.output_file, "w") as text_file:
+        text_file.write(html)
+else:
+    figure.show()
