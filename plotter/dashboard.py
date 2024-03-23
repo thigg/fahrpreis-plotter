68a90549448943ff0f1b0913a87fbb6a833b39fb
thigg <thigg-github@kogge.net>
2024-03-23 19:05:24 +0100

    added caching to dashboard

--- plotter/dashboard.py
+++ plotter/dashboard.py
@@ -1,24 +1,32 @@
 import argparse
+import logging
 
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
+from flask_caching import Cache
 
 from plotter import accumulate_sqlite, plot, get_timeframe, open_sqlite, get_connection_tuples
 
 app = dash.Dash(__name__)
-
+cache = Cache(app.server, config={
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': '/tmp/fahrpreise-cache'
+})
+app.config.suppress_callback_exceptions = True
 
 
 # Define the callback function
 @app.callback(
     Output('output-plot', 'figure'),
-    [Input('x-input', 'value')]
+    [Input('connection-input', 'value')]
 )
-def update_plot(x):
+@cache.memoize(timeout=60*60)
+def update_plot(connection):
+    print(f"rendering connection {connection}")
     conn, cursor = open_sqlite(args.dbfile)
 
-    station1, station2 = x.split("-")
+    station1, station2 = connection.split("-")
     timeframe_start, timeframe_end = get_timeframe(60, 10)
 
     result = accumulate_sqlite(cursor, station1, station2, timeframe_start, timeframe_end)
@@ -30,6 +38,7 @@ def update_plot(x):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='accumulate fahrpreis data')
     parser.add_argument('--dbfile', help='path to the sqlitefile with the data', required=True)
+    parser.add_argument('--port', help='server port', required=True)
     args = parser.parse_args()
     conn, cursor = open_sqlite(args.dbfile)
     connections = [x for x in get_connection_tuples(cursor)]
@@ -38,7 +47,7 @@ if __name__ == '__main__':
     app.layout = html.Div([
         html.Div([
             html.Label("Select Connection:"),
-            dcc.Dropdown(id='x-input', clearable=False,
+            dcc.Dropdown(id='connection-input', clearable=False,
                          options=[{'label': f"{x[0][1]}-{x[1][1]}", 'value': f"{x[0][0]}-{x[1][0]}"} for x in
                                   connections],
                          value=f"{connections[0][0][0]}-{connections[0][1][0]}"),
@@ -47,4 +56,4 @@ if __name__ == '__main__':
         ], style={'width': '100%', 'display': 'inline-block'}),
         dcc.Graph(id='output-plot', style={'height': 'calc(100vh - 150px)'})
     ], style={'height': '100vh'})
-    app.run(debug=True)
+    app.run(port=args.port)
